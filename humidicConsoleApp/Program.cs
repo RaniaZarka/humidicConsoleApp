92bf1701509a001d8f773d8bb6973395a2a94cd3
Rania Zarka <rani0401@edu.easj.dk>
2020-11-30 15:40:18 +0100

    Push again add method and HTTP client to programs.cs

--- humidicConsoleApp/Program.cs
+++ humidicConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -8,20 +10,55 @@ namespace humidicConsoleApp
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
         private const int Port = 12345;
+
         static void Main(string[] args)
         {
-           
+
+            client.BaseAddress = new Uri("https://humidityweb.azurewebsites.net");
+
+            //We need headers for this base address 
+            var val = "application/json";
+
+            // we need to create a new media type that is valide for application jason
+            var media = new MediaTypeWithQualityHeaderValue(val);
+
+            // we need to clear all the default headers
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            // we added the new headers that we defined  
+            client.DefaultRequestHeaders.Accept.Add(media);
+
+            DateTime time = DateTime.Now;
+            try
+            {
+                var humidity = new Humidity(time);
+                int currentMinute = time.Minute;
+
+
+                if (currentMinute % 15 == 0)
+                {
+                    AddHumidityLevel(humidity);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            //User u = new User();
+
 
             using (UdpClient socket = new UdpClient(new IPEndPoint(IPAddress.Any, Port)))
             {
                 IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
-                
 
-                    //Thread.Sleep(2000);
+
+                //Thread.Sleep(2000);
                 while (true)
                 {    //if (datetime.now().minute == (0 or 15 or 30 or 45))
-                    
+
                     Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
                     byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
 
@@ -34,7 +71,17 @@ namespace humidicConsoleApp
             }
 
         }
+
+        private static string AddHumidityLevel(Humidity humidity)
+        {
+            var action = "api/Humidity/Post";
+            var request = client.PostAsJsonAsync(action, humidity);
+
+            var response = request.Result.Content.ReadAsStringAsync();
+            return response.Result;
+
+
+        }
     }
 }
-    
 
